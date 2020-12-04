a832945ed924ac8c4247fd33e27ae1491e65172c
Maksim Merkulov <wardomenmax@gmail.com>
2020-12-04 16:16:46 +0300

    Update les_4_task_2.py

--- les_4_task_2.py
+++ les_4_task_2.py
@@ -7,7 +7,7 @@
 import cProfile
 
 
-def sieve(num):
+def sieve(num,pos):
     prime = [True for i in range(num + 1)]
     p = 2
     num_list = []
@@ -18,7 +18,61 @@ def sieve(num):
         p += 1
     prime[0] = False
     prime[1] = False
-    for p in range(n + 1):
+    for p in range(num + 1):
         if prime[p]:
             num_list.append(p)
-    return num_list
+    return num_list[pos-1]
+
+# 100 loops, best of 5: 4.56 usec per loop "les_4_task_2.sieve(10,3)"
+# 100 loops, best of 5: 18.9 usec per loop "les_4_task_2.sieve(100,10)"
+# 100 loops, best of 5: 17.4 usec per loop "les_4_task_2.sieve(100,25)"
+# 100 loops, best of 5: 231 usec per loop "les_4_task_2.sieve(1000,100)"
+# 100 loops, best of 5: 1.91 msec per loop "les_4_task_2.sieve(10000,200)"
+
+# cProfile.run('sieve(10,3)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:10(sieve)
+# cProfile.run('sieve(100,10)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:10(sieve)
+# cProfile.run('sieve(100,25)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:10(sieve)
+# cProfile.run('sieve(1000,100)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:10(sieve)
+# cProfile.run('sieve(10000,200)')
+# 1    0.005    0.005    0.007    0.007 les_4_task_2.py:10(sieve)
+
+
+def prime(num):
+    count = 1
+    number = 1
+    prime = [2]
+
+    if num == 1:
+        return 2
+
+    while count != num:
+        number += 2
+        for i in prime:
+            if number % i == 0:
+                break
+        else:
+            count += 1
+            prime.append(number)
+
+    return number
+
+# 100 loops, best of 5: 811 nsec per loop "les_4_task_2.sieve(3)"
+# 100 loops, best of 5: 14 usec per loop "les_4_task_2.sieve(10)"
+# 100 loops, best of 5: 26.8 usec per loop "les_4_task_2.sieve(25)"
+# 100 loops, best of 5: 549 usec per loop "les_4_task_2.sieve(100)"
+# 100 loops, best of 5: 1.23 msec per loop "les_4_task_2.sieve(200)"
+
+# cProfile.run('prime(3)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:44(prime)
+# cProfile.run('prime(10)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:44(prime)
+# cProfile.run('prime(25)')
+# 1    0.000    0.000    0.000    0.000 les_4_task_2.py:44(prime)
+cProfile.run('prime(100)')
+# 1    0.001    0.001    0.001    0.001 les_4_task_2.py:44(prime)
+# cProfile.run('prime(200)')
+# 1    0.003    0.003    0.003    0.003 les_4_task_2.py:44(prime)
